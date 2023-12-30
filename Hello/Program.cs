d897f1fad61578d6b290639e4ec2c1edf74e9d8e
xorelorax <allanschwenck@gmail.com>
2023-12-29 23:01:56 -0300

    Hello

---
+++ Hello/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Hello
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
